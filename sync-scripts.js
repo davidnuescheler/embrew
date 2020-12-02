99e44a0ef3ab9ecbb49605184cf6e58dec51b1fa
David Nuescheler <david@nuescheler.ch>
2020-12-02 14:40:10 -0800

    chore: lighthouse optimization

--- sync-scripts.js
+++ sync-scripts.js
@@ -3,13 +3,17 @@ function fixImages() {
     const imgSizes=[375, 768, 1000];
     const fitting=imgSizes.filter(s => s<=screenWidth);
     const width=fitting.length?fitting[fitting.length-1]*2:imgSizes[0]*2;
+    let heroProcessed=false;
     const observer = new MutationObserver(mutations => {
         mutations.forEach(mutation => {
             mutation.addedNodes.forEach(node => {
                 // only handle images with src=/hlx_*
                 // console.log(node.tagName +':'+node.src);
                 if (node.tagName === 'IMG' && node.src.includes('/hlx_') && !node.src.includes('?')) {
+                    const loading=heroProcessed?'lazy':'eager';
+                    heroProcessed=true;
                     node.setAttribute('src', `${node.src}?width=${width}&auto=webp&format=pjpg&optimize=medium`);
+                    node.setAttribute('loading', loading);
                     //console.log('src:'+node.src)
                 }
             });
