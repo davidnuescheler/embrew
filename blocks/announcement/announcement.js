f54a6b6389007e5e488a5e0a08590e3d6157f65f
David Nuescheler <david@nuescheler.ch>
2024-01-04 12:35:34 -0800

    chore: change popup behavior

--- blocks/announcement/announcement.js
+++ blocks/announcement/announcement.js
@@ -1,14 +1,14 @@
 export default async function decorate(block) {
-  if (sessionStorage.getItem('announcement') === 'closed') {
-    block.remove();
-    return;
+  if (sessionStorage.getItem('announcement') !== 'closed') {
+    const close = document.createElement('button');
+    close.classList.add('announcement-close');
+    close.innerHTML = '&times;';
+    close.addEventListener('click', () => {
+      sessionStorage.setItem('announcement', 'closed');
+      block.parentNode.classList.remove('announcement-popped');
+      close.remove();
+    });
+    block.prepend(close);
+    block.parentNode.classList.add('announcement-popped');
   }
-  const close = document.createElement('button');
-  close.classList.add('announcement-close');
-  close.innerHTML = '&times;';
-  close.addEventListener('click', () => {
-    sessionStorage.setItem('announcement', 'closed');
-    block.remove();
-  });
-  block.prepend(close);
 }
